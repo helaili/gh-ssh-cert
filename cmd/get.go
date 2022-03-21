da7791fc23f23db7036d3330c944a610ae6b2e2d
Alain Hélaïli <alain@carbonhub.io>
2022-03-21 13:30:12 +0100

    server is a parameter

--- cmd/get.go
+++ cmd/get.go
@@ -4,9 +4,9 @@ import (
 	"bufio"
 	"bytes"
 	"fmt"
-	"log"
 	"math/rand"
 	"os"
+	"path"
 	"strings"
 	"time"
 
@@ -28,15 +28,15 @@ const randomStringLength = 20
 
 var org string
 var repo string
-var sshKeyName string
-var outputFile string
+var pubKey string
+var serverRootURL string
 
 var getCmd = &cobra.Command{
 	Use:   "get",
 	Short: "Get a new SSH certificate from GitHub",
 	Run: func(cmd *cobra.Command, args []string) {
 		if org == "" && repo == "" {
-			// Not flag was set, let's use the current repo if we are actually in a repo
+			// No flag was set, let's use the current repo if we are actually in a repo
 			currentRepoObj, _ := gh.CurrentRepository()
 			if currentRepoObj != nil {
 				org = currentRepoObj.Owner()
@@ -57,36 +57,48 @@ var getCmd = &cobra.Command{
 			return
 		}
 
-		sshKey, shhKeyError := getSSHKey(sshKeyName)
+		// Get the SSH pub key
+		sshKey, shhKeyError := getSSHKey(pubKey)
 
 		if shhKeyError != nil {
 			fmt.Println(shhKeyError)
 			return
 		}
 
+		// Generate a session token so we can correlate the 2 http calls for generation and retrieval of the certificate
 		sessionToken := randomString(randomStringLength)
 
 		fmt.Printf("Requesting certificate to %s/%s\n", org, repo)
-		err := requestCertificateCreation(org, repo, sshKey, sessionToken)
+		err := requestCertificateCreation(org, repo, sshKey, path.Base(pubKey), sessionToken)
 
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
 
+		// Certificate has been created on the server, let's retrieve it
 		for counter := 0; counter < 10; counter++ {
+			fmt.Printf("Fetching certificate %d/10\n", counter+1)
+
 			cert, error := fetchCertificate(sessionToken)
 			if error == nil {
-				fmt.Printf("Certificate is %s\n", cert)
+				// We got the certificate, let's write it to the file
+				fmt.Println("Certificate succesfully fetched")
+
+				// id_rsa.pub is the public key file name, the cert file is id_rsa-cert.pub
+				outputFile := path.Dir(pubKey) + "/" + strings.TrimSuffix(path.Base(pubKey), ".pub") + "-cert.pub"
+				fmt.Printf("Writing certificate to %s\n", outputFile)
+
 				file, err := os.Create(outputFile)
 				if err != nil {
-					log.Fatalf("Got error while opening the output file. Err: %s", err.Error())
+					fmt.Printf("Got error while opening the output file. Err: %s", err.Error())
 					return
 				}
 				writer := bufio.NewWriter(file)
 				_, err = writer.WriteString(cert)
 				if err != nil {
-					log.Fatalf("Got error while writing to a file. Err: %s", err.Error())
+					fmt.Printf("Got error while writing to a file. Err: %s", err.Error())
+					return
 				}
 				writer.Flush()
 				break
@@ -100,9 +112,10 @@ func init() {
 	rootCmd.AddCommand(getCmd)
 	getCmd.Flags().StringVarP(&org, "org", "o", "", "Organization to use as a certificate authority")
 	getCmd.Flags().StringVarP(&repo, "repo", "r", "", "Repo to use as a certificate authority")
-	getCmd.Flags().StringVarP(&sshKeyName, "key", "k", "", "SSH key to certify")
-	getCmd.Flags().StringVarP(&outputFile, "file", "f", "", "Output file to write the certificate to")
-	getCmd.MarkFlagRequired("file")
+	getCmd.Flags().StringVarP(&pubKey, "pubKey", "k", "", "Public key file")
+	getCmd.MarkFlagRequired("pubKey")
+	getCmd.Flags().StringVarP(&serverRootURL, "url", "u", "", "The SSH Certificate app's root URL")
+	getCmd.MarkFlagRequired("serverRootURL")
 }
 
 func randomString(n int) string {
@@ -115,16 +128,20 @@ func randomString(n int) string {
 	return string(s)
 }
 
-func requestCertificateCreation(org string, repo string, sshKey SSHKey, sessionToken string) error {
+/*
+ * Request the creation of a certificate by sending a repository dispatch event to a repo.
+ * The GitHub App listen to this event and will create a certificate.
+ * This ensure that the current user is authenticated through GitHub and has write access to the repo.
+ */
+func requestCertificateCreation(org string, repo string, sshKey SSHKey, pubKeyFileName string, sessionToken string) error {
 	client, clientError := gh.RESTClient(nil)
 	if clientError != nil {
 		return clientError
 	}
 
-	fmt.Printf("Key is %s\n", sshKey.Key)
-
 	response := struct{ Message string }{}
-	body := fmt.Sprintf(`{"event_type": "certificate-request", "client_payload": {"sessionToken": "%s", "key": "%s", "title": "%s"}}`, sessionToken, sshKey.Key, sshKey.Title)
+	body := fmt.Sprintf(`{"event_type": "certificate-request", "client_payload": {"sessionToken": "%s", "key": "%s", "title": "%s", "pubKeyFileName": "%s"}}`,
+		sessionToken, sshKey.Key, sshKey.Title, pubKeyFileName)
 	bodyReader := bytes.NewReader([]byte(body))
 	postError := client.Post(fmt.Sprintf("repos/%s/%s/dispatches", org, repo), bodyReader, &response)
 
@@ -153,41 +170,47 @@ func getSSHKeys() ([]SSHKey, error) {
 }
 
 /*
- * Retrieve a single SSH key from the current user's GitHub profile
+ * Retrieve locally the user's public key and make sure it exists on their GitHub profile
  */
-func getSSHKey(sshKeyName string) (SSHKey, error) {
-	fmt.Println("Getting ssh keys as none was provided")
-	keys, error := getSSHKeys()
+func getSSHKey(sshKeyFile string) (SSHKey, error) {
+	fmt.Printf("Loading key from file %s\n", sshKeyFile)
 
+	// Read the SSH key from the file
+	content, readSSHKeyFileErr := os.ReadFile(sshKeyFile)
+	if readSSHKeyFileErr != nil {
+		return SSHKey{}, readSSHKeyFileErr
+	}
+
+	sshKey := string(content)
+
+	keys, error := getSSHKeys()
 	if error != nil {
 		return SSHKey{}, error
 	}
 
-	if sshKeyName == "" {
-		if len(keys) == 0 {
-			return SSHKey{}, fmt.Errorf("no SSH keys found on your GitHub profile. Please add one")
-		} else if len(keys) > 1 {
-			return SSHKey{}, fmt.Errorf("%d SSH key were found on your GitHub profile. Use the --key flag to specify which one you want to use", len(keys))
-		} else {
-			return keys[0], nil
-		}
-	} else {
-		for _, key := range keys {
-			if key.Title == sshKeyName {
-				return key, nil
-			}
+	if len(keys) == 0 {
+		return SSHKey{}, fmt.Errorf("no SSH keys found on your GitHub profile. Please add one")
+	}
+
+	// Checking if the public key exists on the user's GitHub profile
+	for _, key := range keys {
+		if strings.HasPrefix(sshKey, key.Key) {
+			fmt.Printf("Found match with key %s on your GitHub profile\n", key.Title)
+			return key, nil
 		}
 	}
-	return SSHKey{}, fmt.Errorf("no SSH key with name %s was found on your GitHub profile. Please add one", sshKeyName)
+
+	return SSHKey{}, fmt.Errorf("no matching SSH key was found on your GitHub profile. Please add one")
 }
 
+/*
+ * Retrieve the certificate from the server
+ */
 func fetchCertificate(sessionToken string) (string, error) {
 	client, clientError := gh.RESTClient(nil)
 	if clientError != nil {
 		return "", clientError
 	}
-
-	fmt.Printf("Fetching certificate\n")
 
 	response := struct {
 		Message     string
@@ -195,7 +218,8 @@ func fetchCertificate(sessionToken string) (string, error) {
 	}{}
 	body := fmt.Sprintf(`{"sessionToken": "%s"}`, sessionToken)
 	bodyReader := bytes.NewReader([]byte(body))
-	postError := client.Post("https://ssh-cert-app.ngrok.io/ssh-cert-app/fetch", bodyReader, &response)
+	url := fmt.Sprintf("%s/fetch", serverRootURL)
+	postError := client.Post(url, bodyReader, &response)
 
 	if postError != nil {
 		return "", postError
